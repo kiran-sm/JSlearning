a722c3b74221a61a95c9fbb0c85d4cd35f4ff62f
Kiran <smkiran.09@gmail.com>
2023-02-03 23:17:51 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const friends = [0,1,2,"asd","asdfg"]
-console.log(friends);
-const years = new Array(1992,1996,2000,2004);
-years[3]="jay"
-console.log(years[years.length-1]);
-console.log(years.map(...friends));
-console.log("test kiran")
