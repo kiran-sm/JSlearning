4b278694038e5037d8aaaff76ef2253dade47ef8
kiran <smkiran.09@gmail.com>
2023-02-03 22:52:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const friends = [0,1,2,"asd","asdfg"]
+console.log(friends);
+const years = new Array(1992,1996,2000,2004);
+years[3]="jay"
+console.log(years[years.length-1]);
+console.log(years.map(...friends));
